270f39af6c246b133ec82b8a441c7ce4c3dd24a3
ShawnDen-coder <shawndeng1109@qq.com>
2024-11-25 11:20:19 +0800

    feat: vue ref class 1

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,46 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <main>
+    <div class="container"><MessageList /></div>
+  </main>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+import MessageList from "./components/MessageList.vue";
+export default {
+  components: {
+    MessageList,
+  },
+  setup() {},
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: Arial, "PingFang SC", "Microsoft Yahei", sans-serif;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+body {
+  background-color: #0f141c;
+  opacity: 1;
+  background-image: radial-gradient(
+    #212943 0.6000000000000001px,
+    #0f141c 0.6000000000000001px
+  );
+  background-size: 12px 12px;
+  color: white;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+#app {
+  width: 100vw;
+  height: 100vh;
+  max-width: 100%;
+  display: grid;
+  place-items: center;
+}
+
+.container {
 }
 </style>
---
+++ src/components/MessageList.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="msg in messages" :key="msg.id">{{ msg.content }}</li>
+    </ul>
+    <button @click="messages = []">删除全部</button>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    const messages = ref([
+      { id: 1, content: "这是一条消息提醒1" },
+      { id: 1, content: "这是一条消息提醒2" },
+      { id: 1, content: "这是一条消息提醒3" },
+    ]);
+
+    return { messages };
+  },
+};
+</script>
+
+<style scoped>
+h2 {
+  color: hsl(280deg, 100%, 70%);
+  margin-bottom: 24px;
+  text-align: center;
+}
+
+ul {
+  list-style: none;
+  display: grid;
+  gap: 24px;
+}
+
+li {
+  display: flex;
+  align-items: center;
+  gap: 12px;
+}
+
+li::before {
+  content: "";
+  display: block;
+  width: 8px;
+  height: 8px;
+  background-color: hsl(280deg, 100%, 70%);
+  border-radius: 100%;
+}
+</style>
