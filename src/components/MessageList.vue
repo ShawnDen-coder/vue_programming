f4849182daae35b123d5c65bb79ec044a4050f0c
ShawnDen-coder <shawndeng1109@qq.com>
2024-11-25 11:51:42 +0800

    feat: reactive 的示例用法

--- src/components/MessageList.vue
+++ src/components/MessageList.vue
@@ -1,14 +1,24 @@
 <template>
   <div>
+    <h2>
+      {{ options.title }}
+    </h2>
+    <p>
+      用户：{{ options.user.name }}, 活跃：{{
+        options.user.active ? "活跃" : "不活跃"
+      }}
+    </p>
     <ul>
       <li v-for="msg in messages" :key="msg.id">{{ msg.content }}</li>
     </ul>
     <button @click="messages = []">删除全部</button>
+    <button @click="options.title = '这是标题'">修改标题</button>
+    <button @click="options.user.name = '李四'">修改用户</button>
   </div>
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref, reactive } from "vue";
 export default {
   setup() {
     const messages = ref([
@@ -16,8 +26,15 @@ export default {
       { id: 1, content: "这是一条消息提醒2" },
       { id: 1, content: "这是一条消息提醒3" },
     ]);
+    const options = reactive({
+      title: "消息列表",
+      user: {
+        name: "张三",
+        active: true,
+      },
+    });
 
-    return { messages };
+    return { messages, options };
   },
 };
 </script>
