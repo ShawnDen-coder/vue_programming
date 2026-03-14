e6b9c9993fc2828d3bad3d33ff0b3300f5c8ef82
ShawnDeng-code <shawndeng1109@qq.com>
2026-03-14 22:29:44 +0800

    feat: add floder example

--- packages/vue-project-floder-structure/src/router/index.ts
+++ packages/vue-project-floder-structure/src/router/index.ts
@@ -1,20 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import type { App } from 'vue'
+import { router } from './router'
 
-export const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: () => import('@/pages/HomePage/index.vue'),
-    },
-    {
-      path: '/articles/:slug',
-      name: 'article-detail',
-      component: () => import('@/pages/ArticlePage/index.vue'),
-    },
-  ],
-  scrollBehavior() {
-    return { top: 0 }
-  },
-})
+export { router }
+
+export function setupRouter(app: App) {
+  app.use(router)
+}
